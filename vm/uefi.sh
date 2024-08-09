779beb33173ff3ecc76ba2ad9a8b061c42cfb6fe
rapiporu <159589341+rapiporu@users.noreply.github.com>
2024-08-08 22:54:08 -0300

    Update uefi.sh

--- vm/uefi.sh
+++ vm/uefi.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-sed -i '/#ParallelDownlaods = 10/c\ParallelDownlaods = 10' /etc/pacman.conf
+sed -i '/#ParallelDownlaods = 5/c\ParallelDownlaods = 10' /etc/pacman.conf
 sed -i '/#VerbosePkgLists/c\VerbosePkgLists' /etc/pacman.conf
 sed -i '/#Color/c\Color' /etc/pacman.conf
-echo -e "ILoveCandy\n" >> /etc/pacman.conf
+#echo -e "ILoveCandy\n" >> /etc/pacman.conf
 timedatectl set-ntp true
 mkfs.ext4 /dev/sda2
 mkfs.fat -F32 /dev/sda1
