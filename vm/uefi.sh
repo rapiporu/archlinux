ab5059a64554db56c62ae06d46667f392a5370a1
rapiporu <159589341+rapiporu@users.noreply.github.com>
2024-08-08 22:39:54 -0300

    Update uefi.sh

--- vm/uefi.sh
+++ vm/uefi.sh
@@ -5,6 +5,6 @@ mkfs.fat -F32 /dev/sda1
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-pacstrap /mnt base linux-zen linux-firmware neovim networkmanager dhcpcd wpa_supplicant grub efibootmgr git
+pacstrap /mnt base linux-zen linux-firmware neovim networkmanager dhcpcd wpa_supplicant grub efibootmgr git sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
